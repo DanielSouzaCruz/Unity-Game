6d563feb2c575764e34f65ecb06e7cbc26230c7f
DanielSouzaCruz <danielrenovar165@gmail.com>
2023-04-25 15:46:31 -0300

    Introdução a Colisões, Powerups

---
+++ Galaxy Shooter/Assets/2D Galaxy Assets/Scripts/Powerup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.0f;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collided with: " + other.name);
+    }
+}
