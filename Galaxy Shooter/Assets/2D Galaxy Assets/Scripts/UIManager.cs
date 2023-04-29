ad1c67cafe6eafc8535955ab78c14478d8fd2a72
DanielSouzaCruz <danielrenovar165@gmail.com>
2023-04-29 13:29:09 -0300

    Introdução a Interface de Usuario UI

---
+++ Galaxy Shooter/Assets/2D Galaxy Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    public Sprite[] lives;
+
+    // Start is called before the first frame update
+    public void UpdateLives()
+    {
+        
+    }
+
+    public void UpdateScore()
+    {
+
+    }
+}
